d7385a9c08e994df5ca23baa5ce75da931ccd87a
dach83 <dach83@mail.ru>
2022-05-31 09:44:19 +0300

    Show current location

--- app/src/main/java/com/chernorotov/placebook/MapsActivity.kt
+++ app/src/main/java/com/chernorotov/placebook/MapsActivity.kt
@@ -1,20 +1,29 @@
 package com.chernorotov.placebook
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.chernorotov.placebook.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.chernorotov.placebook.databinding.ActivityMapsBinding
+import com.google.android.gms.tasks.Task
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +35,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        setupLocationClient()
     }
 
     /**
@@ -38,11 +49,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        getCurrentLocation()
     }
+
+    private fun setupLocationClient() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun checkLocationPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_LOCATION -> onRequestLocationPermissionResult(grantResults)
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun onRequestLocationPermissionResult(grantResults: IntArray) {
+        if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation()
+        } else {
+            Log.e(TAG, "Location permission denied")
+        }
+    }
+
+    private fun onCompleteLastLocation(lastLocationTask: Task<Location>) {
+        val location = lastLocationTask.result
+        if (location != null) {
+            val latLng = LatLng(location.latitude, location.longitude)
+            Log.d(TAG, "onCompleteLastLocation: $latLng")
+            map.addMarker(MarkerOptions().position(latLng).title("You are here"))
+            val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+            map.moveCamera(update)
+        } else {
+            Log.e(TAG, "No location found")
+        }
+    }
+
+    private fun getCurrentLocation() {
+        if (!checkLocationPermission()) {
+            requestLocationPermission()
+        } else {
+            fusedLocationClient.lastLocation.addOnCompleteListener(::onCompleteLastLocation)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1
+        private const val TAG = "MainActivity"
+    }
+
 }
